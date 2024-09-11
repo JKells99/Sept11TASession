6783b587dea8d6a52619a05136a7fb290de0a2e0
Jordan <jordan.kelloway@keyin.com>
2024-09-11 11:20:13 -0230

    Added a couple files

--- src/main/java/com/keyin/Hello.java
+++ src/main/java/com/keyin/Hello.java
@@ -1,7 +1,12 @@
 package com.keyin;
 
+import com.keyin.animals.Animal;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Animal animal = new Animal();
+        animal.setName("Jimmy");
     }
 }
